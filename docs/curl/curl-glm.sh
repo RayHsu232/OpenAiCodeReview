21c4b10d6396b338a3214475086ced6eb5b10172
RayHsu232 <ruixu1114@gmail.com>
2025-10-15 19:38:09 +0800

    feat:代码评审

---
+++ docs/curl/curl-glm.sh
@@ -0,0 +1,15 @@
+curl -X POST \
+        -H "Authorization: Bearer 59de6d62732748fbba0f7503843c3506.9MixyWalBKHC96Pr" \
+        -H "Content-Type: application/json" \
+        -H "User-Agent: Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)" \
+        -d '{
+          "model":"glm-4",
+          "stream": "true",
+          "messages": [
+              {
+                  "role": "user",
+                  "content": "1+1"
+              }
+          ]
+        }' \
+  https://open.bigmodel.cn/api/paas/v4/chat/completions
