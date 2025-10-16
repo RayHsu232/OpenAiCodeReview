9b30735041fcf21815d4213bd1d5ae0ea238787e
RayHsu232 <ruixu1114@gmail.com>
2025-10-16 14:32:25 +0800

    feat:代码评审v4.3

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -1,20 +1,16 @@
 package plus.gaga.middleware.sdk;
 
 import com.alibaba.fastjson2.JSON;
-import plus.gaga.middleware.sdk.domain.model.ChatCompletionRequest;
 import plus.gaga.middleware.sdk.domain.model.ChatCompletionSyncResponse;
-import plus.gaga.middleware.sdk.domain.model.Model;
 import plus.gaga.middleware.sdk.types.utils.BearerTokenUtils;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 public class OpenAiCodeReview {
 
@@ -40,59 +36,58 @@ public class OpenAiCodeReview {
 
         System.out.println("diff code：" + diffCode.toString());
 
-        // 2. chatglm 代码评审
+        // 2.ChatGLM 代码评审
         String log = codeReview(diffCode.toString());
-        System.out.println("code review：" + log);
+        System.out.println("code review:" + log);
     }
 
-    private static String codeReview(String diffCode) throws Exception {
-
-        String apiKeySecret = "59de6d62732748fbba0f7503843c3506.9MixyWalBKHC96Pr";
-        String token = BearerTokenUtils.getToken(apiKeySecret);
+    private static String codeReview(String diffCode) throws Exception{
+        String ApiKeySecret = "59de6d62732748fbba0f7503843c3506.9MixyWalBKHC96Pr";
+        String token = BearerTokenUtils.getToken(ApiKeySecret);
 
         URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Authorization", "Bearer " + token);
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-        connection.setDoOutput(true);
+        connection .setDoOutput(true);
 
-        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
-        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
-        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
-            private static final long serialVersionUID = -7988151926241837899L;
+        String code = diffCode;
 
-            {
-                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
-                add(new ChatCompletionRequest.Prompt("user", diffCode));
-            }
-        });
+        String JsonInputString = "{"
+                + "\"model\":\"glm-4-flash\","
+                + "\"messages\": ["
+                + "    {"
+                + "        \"role\": \"user\","
+                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
+                + "    }"
+                + "]"
+                + "}";
 
-        try (OutputStream os = connection.getOutputStream()) {
-            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
+        try(OutputStream os = connection.getOutputStream()){
+            byte[] input = JsonInputString.getBytes(StandardCharsets.UTF_8);
             os.write(input);
-        }
+        };
 
         int responseCode = connection.getResponseCode();
         System.out.println(responseCode);
 
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String inputLine;
-
         StringBuilder content = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
+        while((inputLine = in.readLine()) != null) {
             content.append(inputLine);
         }
 
         in.close();
         connection.disconnect();
 
-        System.out.println("评审结果：" + content.toString());
+        System.out.println(content);
 
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
     }
+
 
 }
