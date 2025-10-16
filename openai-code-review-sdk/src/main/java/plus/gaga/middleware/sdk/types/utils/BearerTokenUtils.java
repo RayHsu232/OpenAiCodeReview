50b3b469a2b52ad8b4ceda60f12442c259f996c9
RayHsu232 <ruixu1114@gmail.com>
2025-10-16 14:47:39 +0800

    feat:代码评审v4.1

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/types/utils/BearerTokenUtils.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/types/utils/BearerTokenUtils.java
@@ -12,12 +12,13 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class BearerTokenUtils {
-    // 过期时间
+
+    // 过期时间；默认30分钟
     private static final long expireMillis = 30 * 60 * 1000L;
 
     // 缓存服务
     public static Cache<String, String> cache = CacheBuilder.newBuilder()
-            .expireAfterWrite(expireMillis - (60 * 1000L), TimeUnit.MICROSECONDS)
+            .expireAfterWrite(expireMillis - (60 * 1000L), TimeUnit.MILLISECONDS)
             .build();
 
     public static String getToken(String apiKeySecret) {
@@ -26,10 +27,11 @@ public class BearerTokenUtils {
     }
 
     /**
+     * 对 ApiKey 进行签名
      *
-     * @param apiKey
-     * @param apiSecret
-     * @return
+     * @param apiKey    登录创建 ApiKey <a href="https://open.bigmodel.cn/usercenter/apikeys">apikeys</a>
+     * @param apiSecret apiKey的后半部分 828902ec516c45307619708d3e780ae1.w5eKiLvhnLP8MtIf 取 w5eKiLvhnLP8MtIf 使用
+     * @return Token
      */
     public static String getToken(String apiKey, String apiSecret) {
         // 缓存Token
@@ -47,6 +49,6 @@ public class BearerTokenUtils {
         token = JWT.create().withPayload(payload).withHeader(headerClaims).sign(algorithm);
         cache.put(apiKey, token);
         return token;
-
     }
+
 }
